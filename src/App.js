5659ef20b2dfcaefd8ff909beb70d26829ac15c0
Caroline Nunes <nunescaroline905@gmail.com>
2023-05-03 21:49:11 -0300

    feat: adding config

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { Component } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Game from './pages/Game'
 
-export default App;
+class App extends Component {
+  render() {
+    return (
+      <main>
+        <Switch>
+          <Route component={ Game } />
+        </Switch>
+      </main>
+    )
+  }
+}
+export default App
